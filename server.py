b5dfb83010605345730111ad91d25fde99db261d
John Wiseheart <johnwiseheart@gmail.com>
2016-02-27 00:32:11 +1100

    Memes

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, abort, request, Response
+from flask import Flask, render_template, jsonify, abort, request, Response, redirect, session, url_for, g
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
@@ -6,35 +6,49 @@ from flask.ext.classy import FlaskView
 import requests
 from datetime import datetime
 import json
+from flask_oauth import OAuth
+from urllib2 import Request, urlopen, URLError
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = "Memes"
+REDIRECT_URI = "/oauth2callback"
 db = SQLAlchemy(app)
 admin = Admin(app, name='tops', template_mode='bootstrap3')
+oauth = OAuth()
+app.config.update(
+    GOOGLE_CONSUMER_KEY='655313797231-1m1s6aqi70aedl9k6bh6h3trbpge8t6r.apps.googleusercontent.com',
+    GOOGLE_CONSUMER_SECRET='i9N3ZAmve-5ljeDcBoSDXHyA',
+    SECRET_KEY='just a secret key, to confound the bad guys',
+    DEBUG=True
+)
+google = oauth.remote_app('google',
+                          base_url='https://www.google.com/accounts/',
+                          authorize_url='https://accounts.google.com/o/oauth2/auth',
+                          request_token_url=None,
+                          request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
+                                                'response_type': 'code'},
+                          access_token_url='https://accounts.google.com/o/oauth2/token',
+                          access_token_method='POST',
+                          access_token_params={'grant_type': 'authorization_code'},
+                          consumer_key=app.config['GOOGLE_CONSUMER_KEY'],
+                          consumer_secret=app.config['GOOGLE_CONSUMER_SECRET'])
 
-def authenticate():
-    url = "https://app.pingboard.com/oauth/token"
-    querystring = {"grant_type":"client_credentials"}
-    payload = "client_id=698fb86824586a1b700e882933c966968e60c2061f529b146f2977bd43372ec8&client_secret=20ed6a86a904a1e76d4129b77dd2b1439733910f7a53f6bd60f37d9ca46557ba"
-    headers = {
-        'content-type': "application/x-www-form-urlencoded"
-    }
 
-    response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
-
-    data = json.loads(response.text)
-    return data['access_token']
-
-access_token = authenticate()
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String)
+    tops = db.Column(db.Integer)
+    total_tops = db.Column(db.Integer)
 
 class TopsEvent(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     reason = db.Column(db.String(256))
     approved = db.Column(db.Boolean, default=True)
-    to_user_name = db.Column(db.String)
-    from_user_name = db.Column(db.String)
+    to_user_email = db.Column(db.String)
+    from_user_email = db.Column(db.String)
     datetime = db.Column(db.DateTime, default=datetime.now())
 
     def __repr__(self):
@@ -45,14 +59,15 @@ class TopsEvent(db.Model):
 
 class RedeemEvent(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', backref="redeem_events")
     redeemable_id = db.Column(db.Integer, db.ForeignKey('redeemable.id'))
     redeemable = db.relationship("Redeemable", backref="redeem_events")
     datetime = db.Column(db.DateTime, default=datetime.now())
     approved = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
-        return '<RedeemableEvent user:%r redeemable_id:%r>' % (self.user_name, self.redeemable_id)
+        return '<RedeemableEvent user:%r redeemable_id:%r>' % (self.user_id, self.redeemable_id)
 
     def to_dict(self):
        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
@@ -70,19 +85,101 @@ class Redeemable(db.Model):
     def to_dict(self):
        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
-admin.add_view(ModelView(TopsEvent, db.session))
-admin.add_view(ModelView(RedeemEvent, db.session))
-admin.add_view(ModelView(Redeemable, db.session))
+
 
 @app.route('/')
 def index():
+    access_token = session.get('access_token')
+    if access_token is None:
+        return redirect(url_for('login'))
+    access_token = access_token[0]
+    headers = {'Authorization': 'OAuth '+access_token}
+    req = Request('https://www.googleapis.com/oauth2/v1/userinfo',
+                  None, headers)
+    try:
+        res = urlopen(req)
+    except URLError, e:
+        if e.code == 401:
+            # Unauthorized - bad token
+            session.pop('access_token', None)
+            return redirect(url_for('login'))
+        return res.read()
+    session['email'] = json.loads(res.read())['email']
+    user = User.query.filter_by(email=session['email']).first()
+    if not user:
+        user = User(
+            email=session['email'],
+            tops=0,
+            total_tops=0
+        )
+        db.session.add(user)
+        db.session.commit()
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    callback=url_for('authorized', _external=True)
+    return google.authorize(callback=callback)
+
+@app.route('/logout')
+def logout():
+    session.pop('access_token')
+    return redirect(url_for('login'))
+
+@app.route(REDIRECT_URI)
+@google.authorized_handler
+def authorized(resp):
+    access_token = resp['access_token']
+    session['access_token'] = access_token, ''
+    return redirect(url_for('index'))
+
+@google.tokengetter
+def get_access_token():
+    return session.get('access_token')
+
+
+
+def authenticate():
+    url = "https://app.pingboard.com/oauth/token"
+    querystring = {"grant_type":"client_credentials"}
+    payload = "client_id=698fb86824586a1b700e882933c966968e60c2061f529b146f2977bd43372ec8&client_secret=20ed6a86a904a1e76d4129b77dd2b1439733910f7a53f6bd60f37d9ca46557ba"
+    headers = {
+        'content-type': "application/x-www-form-urlencoded"
+    }
+
+    response = requests.request("POST", url, data=payload, headers=headers, params=querystring)
+
+    data = json.loads(response.text)
+    return data['access_token']
+
+access_token = authenticate()
+
+
+admin.add_view(ModelView(TopsEvent, db.session))
+admin.add_view(ModelView(RedeemEvent, db.session))
+admin.add_view(ModelView(Redeemable, db.session))
+admin.add_view(ModelView(User, db.session))
+
+@app.route('/redeem')
+def redeem():
+    print session['email']
+    user = User.query.filter_by(email=session['email']).first()
+    if not user:
+        return redirect(url_for('login'))
+    redeemables = Redeemable.query.all()
+    return render_template('redeem.html', redeemables=redeemables, points=user.tops)
 
 @app.route('/resetdb')
 def resetdb():
     db.create_all()
     return "Done"
 
+@app.route('/points/')
+def points(email):
+    user = User.query.filter_by(email=email).first()
+    if not user:
+        abort(404)
+    return jsonify(tops=user.tops, total_tops=user.total_tops)
 
 # API
 class UsersView(FlaskView):
@@ -99,7 +196,7 @@ class UsersView(FlaskView):
 
         result = []
         for x in response.json()['hits']['hits']:
-            print x
+            # print x
             result.append({
                 "score": x['_score'],
                 "name": x['_source']['first_name'] + ' ' + x['_source']['last_name'],
@@ -130,39 +227,30 @@ class UsersView(FlaskView):
                         mimetype="application/json")
 
 
-class RedeemablesView(FlaskView):
-    def index(self):
-        redeemables = [x.to_dict() for x in Redeemable.query.all()]
-        return jsonify(data=redeemables)
-
-    def get(self, id):
-        redeemable = Redeemable.query.get(id)
-        if not redeemable:
-            abort(404)
-        return jsonify(redeemable.to_dict())
-
 class RedeemEventsView(FlaskView):
     def index(self):
-        events = [x.to_dict() for x in RedeemableEvent.query.all()]
+        events = [x.to_dict() for x in RedeemEvent.query.all()]
         return jsonify(data=events)
 
     def get(self, id):
-        event = RedeemableEvent.query.get(id)
+        event = RedeemEvent.query.get(id)
         if not event:
             abort(404)
         return jsonify(event.to_dict())
 
     def post(self):
-        print [x.to_dict() for x in Redeemable.query.all()]
-        print request.json['redeemable_id']
-
+        # print [x.to_dict() for x in Redeemable.query.all()]
+        # print request.json['redeemable_id']
+        user = User.query.filter_by(email=session['email']).first()
         redeemable = Redeemable.query.get(request.json['redeemable_id'])
         if not redeemable:
             abort(404)
         event = RedeemEvent(
-            user_name=request.json['user_name'],
+            user=user,
             redeemable=redeemable,
         )
+        user.tops -= 1;
+        db.session.add(user)
         db.session.add(event)
         db.session.commit()
         return jsonify(success=True)
@@ -176,18 +264,27 @@ class TopsEventsView(FlaskView):
         return jsonify(data=events)
 
     def get(self, id):
-        event = RedeemableEvent.query.get(id)
+        event = RedeemaEvent.query.get(id)
         if not event:
             abort(404)
         return jsonify(event.to_dict())
 
     def post(self):
-        print request.json
+        user = User.query.filter_by(email=request.json['to_user_email']).first()
+        if not user:
+            user = User(
+                email=request.json['to_user_email'],
+                tops=0,
+                total_tops=0
+            )
+        user.tops += 1
+        user.total_tops += 1
         event = TopsEvent(
             reason=request.json['reason'],
-            to_user_name=request.json['to_user_name'],
-            from_user_name=request.json['from_user_name'],
+            to_user_email=request.json['to_user_email'],
+            from_user_email=session['email'],
         )
+        db.session.add(user)
         db.session.add(event)
         db.session.commit()
         return jsonify(success=True)
@@ -195,7 +292,6 @@ class TopsEventsView(FlaskView):
     def put(self, id):
         pass
 
-RedeemablesView.register(app)
 RedeemEventsView.register(app)
 TopsEventsView.register(app)
 UsersView.register(app)
